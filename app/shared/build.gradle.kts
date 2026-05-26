020e8333e02f59123834bb7944b3f319288dfff2
dpinilla <ingdiegopinilla@hotmail.com>
2026-05-26 17:29:26 -0500

    Cambio 9 en el build.gradle.kts

--- app/shared/build.gradle.kts
+++ app/shared/build.gradle.kts
@@ -73,7 +73,7 @@ kotlin {
             /*
              Navigation
              */
-            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.8.0") // Versión oficial multiplataforma
+            implementation("org.jetbrains.androidx.navigation:navigation-compose:2.9.2") // Versión oficial multiplataforma
 
 
         }
